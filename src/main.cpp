d974bcad475c5d6a56d51bf1754970d2641678c5
Fei Zheng <feizheng@FeideMacBook-Pro.local>
2017-10-15 17:18:01 +0800

    avoid collision

--- src/main.cpp
+++ src/main.cpp
@@ -240,6 +240,37 @@ int main() {
 
             int prev_size = previous_path_x.size();
 
+            if(prev_size > 0)
+            {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            // find ref_v to use
+            for (int i = 0; i < sensor_fusion.size(); i++)
+            {
+              // car is in my lane?
+              double d = sensor_fusion[i][6];
+              if(d < (2 + 4 * lane + 2) && d > (2 + 4 * lane -2))
+              {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                // project s value in 1 timestep
+                check_car_s += ((double)prev_size * 0.02 * check_speed);
+                // check the sensor fusion car is in front of our car and the distance is less than 30 meters
+                if((check_car_s > car_s) && ((check_car_s - car_s) < 30))
+                {
+                  // take action: reduce speed
+                  // ref_vel = 29.5;
+                  too_close = true;
+                }
+              }
+            }
+
             vector<double> ptsx;
             vector<double> ptsy;
 
